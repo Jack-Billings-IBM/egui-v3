3a741a77af4dbb86d8f36fae83c6b44869d2affd
Jack Billings <jack.billings@ibm.com>
2020-08-20 13:22:36 -0500

    updated basic authorization and API URLs

--- public/javascripts/item-list/item-list.component.js
+++ public/javascripts/item-list/item-list.component.js
@@ -9,7 +9,11 @@ angular
       var self = this;
       self.orderProp = 'description';
 
-      $http.get('http://cap-sg-prd-2.integration.ibmcloud.com:16598/catalog/items?startItemID=0010')
+      $http.get('http://cap-sg-prd-2.integration.ibmcloud.com:16221/catalog/items?startItemID=0010',
+        {
+          headers: {Authorization: 'Basic YmtlbGxlcjpwYXNzdzByZA=='},
+        }          
+      )
         .then(function(response) {
 
           self.items = response.data.cics_cat_resp.inquire_request.cat_item;
